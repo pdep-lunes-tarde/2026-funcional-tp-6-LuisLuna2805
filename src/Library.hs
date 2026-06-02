0068cf06815a8991b6e3a1e21b3711e070f9cc9a
LuchiLuna <luiluna@frba.utn.edu.ar>
2026-06-02 17:51:49 -0300

    Completo

--- src/Library.hs
+++ src/Library.hs
@@ -1,20 +1,138 @@
 module Library where
 import PdePreludat
+import GHC.InfoProv (InfoProv)
+import GHC.Num (integerAnd)
 
 data Ingrediente =
-    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
+    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras | Papas | PatyVegano | Tofu | PanIntegral
     deriving (Eq, Show)
 
 precioIngrediente Carne = 20
-precioIngrediente Pan = 2
-precioIngrediente Panceta = 10
-precioIngrediente Cheddar = 10
 precioIngrediente Pollo =  10
-precioIngrediente Curry = 5
+precioIngrediente PatyVegano = 10
+precioIngrediente Pan = 2
+precioIngrediente PanIntegral = 3
+precioIngrediente Panceta = 10
+precioIngrediente Tofu = 10
+precioIngrediente Cheddar = 10
 precioIngrediente QuesoDeAlmendras = 15
+precioIngrediente Curry = 5
+precioIngrediente Papas = 10
 
 data Hamburguesa = Hamburguesa {
     precioBase :: Number,
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
+
+
+--Parte 1 
+cuartoDeLibra :: Hamburguesa 
+cuartoDeLibra = Hamburguesa {
+    precioBase = 20,
+    ingredientes = [Pan, Carne, Cheddar, Pan]
+}
+
+pdepBurger :: Hamburguesa
+pdepBurger =  agrandarMultiple 2 . agregarIngredientes [Panceta, Cheddar] $ cuartoDeLibra
+
+--Caclula el precio de los ingredientes de una hamburguesa
+precioIngredientes :: [Ingrediente] -> Number
+precioIngredientes [] = 0
+precioIngredientes (x:xs) = precioIngrediente x + precioIngredientes xs 
+
+--Calcula precio final de una hamburguesa
+calculoNormal :: Hamburguesa -> Number
+calculoNormal hamburguesa = precioBase hamburguesa + precioIngredientes (ingredientes hamburguesa)
+
+descuento :: Number -> Hamburguesa -> Number
+descuento porcentaje hamburguesa = (calculoNormal hamburguesa) * (100 - porcentaje)/100
+
+--Calcula precio final de una hamburguesa
+precioFinal :: Hamburguesa -> Number
+precioFinal hamburguesa
+    |hamburguesa == pdepBurger = descuento 20 pdepBurger
+    |elem Papas (ingredientes hamburguesa) = descuento 30 hamburguesa --si en algun momento otro pide papas tambien le aplicaria (mal pero no se me ocurre otra cosa)
+    |otherwise = calculoNormal hamburguesa
+
+--Definir que medallon tiene primero
+detectarMdeallon :: [Ingrediente] -> Ingrediente
+detectarMdeallon [] = undefined
+detectarMdeallon (x:xs)   
+    |   x == Carne  = Carne
+    |   x == Pollo  = Pollo
+    |   x == PatyVegano = PatyVegano
+    |   otherwise = detectarMdeallon xs  
+
+--Agregarle un medallon del que ya tenga (si hay mas de uno el primero que vea)
+agrandar :: Hamburguesa -> Hamburguesa
+agrandar hamburguesa = agregarIngrediente (detectarMdeallon (ingredientes hamburguesa)) hamburguesa
+
+agrandarMultiple :: Number -> Hamburguesa -> Hamburguesa
+agrandarMultiple 0 hamburguesa = hamburguesa
+agrandarMultiple cantidad hamburguesa = agrandarMultiple (cantidad-1) (agrandar hamburguesa)
+
+--Agregar un ingrediente cualquiera a mi hamburguesa
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente nuevoIngrediente hamburguesa = 
+    hamburguesa {ingredientes = ingredientes hamburguesa ++ [nuevoIngrediente]}
+
+agregarIngredientes :: [Ingrediente] -> Hamburguesa -> Hamburguesa
+agregarIngredientes [] hamburguesa = hamburguesa
+agregarIngredientes (x:xs) hamburguesa = agregarIngredientes xs (agregarIngrediente x hamburguesa)
+
+agregarIngredientesPorCantidad :: [Ingrediente] -> Number -> Hamburguesa -> Hamburguesa
+agregarIngredientesPorCantidad [] _ hamburguesa = hamburguesa
+agregarIngredientesPorCantidad [x] 0 hamburguesa = hamburguesa
+agregarIngredientesPorCantidad (x:xs) numero hamburguesa = agregarIngredientesPorCantidad (x:xs) (numero - 1) (agregarIngrediente x hamburguesa)
+
+--Parte 2
+dobleCuarto :: Hamburguesa
+dobleCuarto =  agrandar . agregarIngrediente Cheddar $ cuartoDeLibra
+
+bigPdep :: Hamburguesa
+bigPdep =  agregarIngrediente Curry dobleCuarto
+
+--Le agrega papas y descuento del 30 a una hamburguesa
+delDia :: Hamburguesa -> Hamburguesa
+delDia hamburguesa = agregarIngrediente Papas hamburguesa --Y CAMBIARLE EL NOMBRE
+
+--Parte 3
+-- 
+
+reemplazo :: Ingrediente -> Ingrediente 
+reemplazo ingredienteViejo 
+    |   ingredienteViejo == Cheddar = QuesoDeAlmendras
+    |   ingredienteViejo == Pollo = PatyVegano
+    |   ingredienteViejo == Carne = PatyVegano
+    |   ingredienteViejo == Panceta = Tofu
+    |   ingredienteViejo == Pan = PanIntegral
+    |   otherwise = undefined
+
+cuantosSaque :: Hamburguesa -> Hamburguesa -> Number
+cuantosSaque original filtrada = length (ingredientes original) - length (ingredientes filtrada) 
+
+sacarIngrediente:: Ingrediente -> Hamburguesa -> Hamburguesa
+sacarIngrediente viejoIngrediente hamburguesa = 
+    hamburguesa  {ingredientes = filter (/= viejoIngrediente) (ingredientes hamburguesa)}
+
+esNecesarioAgregar :: Ingrediente -> Hamburguesa -> [Ingrediente]
+esNecesarioAgregar ingrediente hamburguesa
+    |elem ingrediente (ingredientes hamburguesa) = [reemplazo ingrediente]  
+    |otherwise = []
+
+cambiarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa 
+cambiarIngrediente ingrediente hamburguesa = 
+    agregarIngredientesPorCantidad (esNecesarioAgregar ingrediente hamburguesa) (cuantosSaque hamburguesa (sacarIngrediente ingrediente hamburguesa)) (sacarIngrediente ingrediente hamburguesa) 
+
+hacerVeggie :: Hamburguesa -> Hamburguesa
+hacerVeggie = cambiarIngrediente Cheddar . cambiarIngrediente Panceta . cambiarIngrediente Carne . cambiarIngrediente Pollo
+
+-- cambiar pon pan integral -> Precio 3
+cambiarPanDePaty :: Hamburguesa -> Hamburguesa
+cambiarPanDePaty = cambiarIngrediente Pan
+-- falta considerar la existencia de mas de uno (con cada elemento lo mismo) (contar la cantidad que saca y agregar esa misma cantidad)
+
+-- doble cuarto pero veggie con pan integral
+dobleCuartoVegano :: Hamburguesa
+dobleCuartoVegano = cambiarPanDePaty (hacerVeggie dobleCuarto) 
 
--- src/Spec.hs
+++ src/Spec.hs
@@ -7,5 +7,46 @@ import Control.Exception (evaluate)
 correrTests :: IO ()
 correrTests = hspec $ do
     describe "TP 5" $ do
-        it "test de prueba" $ do
-            2 + 2 `shouldBe` 4
+        describe "Parte 1" $ do
+            it "precio final de cuartoDeLibra deberia ser 54" $ do
+                precioFinal cuartoDeLibra `shouldBe` 54
+            it "precio final de pdepBurger deberia ser 91.2" $ do
+                precioFinal pdepBurger `shouldBe` 91.2
+            it "agrandar hamburguesa deberia sumarle un medallon del que tenga" $ do
+                agrandar cuartoDeLibra `shouldBe` Hamburguesa {precioBase = 20, ingredientes = [Pan,Carne,Cheddar,Pan,Carne]}
+            it "agrandar varias veces hamburguesa deberia sumar tantos medaloones como se indique" $ do
+                agrandarMultiple 3 cuartoDeLibra `shouldBe` Hamburguesa {precioBase = 20, ingredientes = [Pan,Carne,Cheddar,Pan,Carne,Carne,Carne]}
+            it "agregar ingrediente deberia sumar un ingrediente de cualquier tipo " $ do
+                agregarIngrediente Panceta cuartoDeLibra `shouldBe` Hamburguesa {precioBase = 20, ingredientes = [Pan,Carne,Cheddar,Pan,Panceta]}
+            it "agregar ingredientes Multiples deberia sumar tantos ingredientes como se indique de cualquier tipo " $ do
+                agregarIngredientes [Panceta, Cheddar, Pollo, Curry] cuartoDeLibra `shouldBe` Hamburguesa {precioBase = 20, ingredientes = [Pan,Carne,Cheddar,Pan,Panceta, Cheddar, Pollo, Curry]}
+            it "aplicar descuento deberia realizar un descuento sobre el precioFinal" $ do
+                descuento 50 cuartoDeLibra `shouldBe` 27
+
+        describe "Parte 2" $ do
+            it "ingredientes dobleCuarto deberia mostrar la composicion de la hamburguesa" $ do
+                ingredientes dobleCuarto `shouldBe` [Pan,Carne,Cheddar,Pan,Cheddar,Carne]
+            it "precio dobleCuarto deberia ser 84" $ do
+                precioFinal dobleCuarto `shouldBe` 84
+            it "bigPdep deberia mostrar la composicion de la hamburguesa" $ do
+                ingredientes bigPdep `shouldBe` [Pan,Carne,Cheddar,Pan,Cheddar,Carne,Curry]
+            it "precio bigPdep deberia ser 89" $ do
+                precioFinal bigPdep `shouldBe` 89
+            it "ingredientes delDia bigPdep deberia mostrar la composicion del combo" $ do
+                ingredientes (delDia bigPdep) `shouldBe` [Pan,Carne,Cheddar,Pan,Cheddar,Carne,Curry,Papas]
+            it "precio delDia bigPdep deberia ser 69.3 porque tiene 30% off" $ do
+                precioFinal (delDia bigPdep) `shouldBe` 69.3
+        
+        describe "Parte 3" $ do
+            it "hacer veggie dobleCuarto deberia volver las partes del combo veganas" $ do
+                hacerVeggie dobleCuarto `shouldBe` Hamburguesa {precioBase = 20, ingredientes = [Pan,Pan,PatyVegano,PatyVegano,QuesoDeAlmendras,QuesoDeAlmendras]}
+            it "hacer veggie bigPdep deberia volver las partes del combo veganas" $ do
+                hacerVeggie bigPdep `shouldBe` Hamburguesa {precioBase = 20, ingredientes = [Pan,Pan,Curry,PatyVegano,PatyVegano,QuesoDeAlmendras,QuesoDeAlmendras]}
+            it "hacer veggie dobleCuartoVegano deberia quedarse igual" $ do
+                hacerVeggie dobleCuartoVegano `shouldBe` dobleCuartoVegano
+            it "cambiar pan de paty deberia cambiar el pan por integral" $ do
+                cambiarPanDePaty bigPdep `shouldBe` Hamburguesa {precioBase = 20, ingredientes = [Carne,Cheddar,Cheddar,Carne,Curry,PanIntegral,PanIntegral]}
+            it "ingredientes dobleCuartoVegano deberia mostrar la composicion de la hamburguesa" $ do
+                ingredientes dobleCuartoVegano `shouldBe` [PatyVegano,PatyVegano,QuesoDeAlmendras,QuesoDeAlmendras,PanIntegral,PanIntegral]
+            it "precio dobleCuartoVegano deberia ser 76" $ do
+                precioFinal dobleCuartoVegano `shouldBe` 76
